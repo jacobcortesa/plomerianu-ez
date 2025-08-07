29f324edd00b6ba6a0b15dc8d5518462e0d3b832
Jacob Cortés <jacobca03@gmail.com>
2025-08-07 13:05:23 -0500

    agregando mejora de pagina de cotizacion

--- cotizacion.js
+++ cotizacion.js
@@ -102,3 +102,14 @@ document.addEventListener("DOMContentLoaded", () => {
     )}`;
   }
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", function () {
+      this.classList.toggle("active");
+      navLinks.classList.toggle("active");
+    });
+  }
+});
